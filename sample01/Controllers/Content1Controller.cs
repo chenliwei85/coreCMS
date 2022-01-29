751665686504832b420d75fc1e259054cfe457a4
chenwei <croamman@hotmail.com>
2022-01-29 16:41:25 +0800

    添加cms代码

---
+++ sample01/Controllers/Content1Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sample01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample01.Controllers
+{
+    public class Content1Controller : Controller
+    {
+        private readonly Content contents;
+        public Content1Controller(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+        public IActionResult Index()
+        {
+            return View(new ContentViewModel { Contents = new List<Content> { contents } });
+        }
+    }
+}
